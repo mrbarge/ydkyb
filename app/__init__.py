398d4f0f5a24d34409fbdb54c2a64f3a3cc7fff7
Matt Bargenquast <nobargohere@gmail.com>
2014-07-08 21:36:04 +1000

    Adding base version.

---
+++ app/__init__.py
@@ -0,0 +1,46 @@
+from flask import Flask, session, redirect, url_for, request, render_template
+from app.ydkbengine import ydkbengine
+from random import shuffle
+import os
+
+app = Flask(__name__)
+app.config.from_object('app.config')
+# test the API key for steam has been set
+if not app.config['STEAM_API_KEY']:
+	raise Exception("Must define STEAM_API_KEY in application config.")
+
+@app.route('/', methods=['GET','POST'])
+@app.route('/index',methods=['GET','POST'])
+def index():
+	if request.method == 'POST':
+		session['username'] = request.form['username']
+		app.ydkbengine = ydkbengine(app.config['STEAM_API_KEY'],session['username'])
+		return redirect(url_for('play'))
+	else:
+		return render_template('index.html')
+
+@app.route('/play',methods=['GET','POST'])
+def play():
+	if request.method == 'GET':
+		ownedgame = app.ydkbengine.pickOwnedGame(0)
+		othergames = app.ydkbengine.pickOtherGames(2)
+		# put all games in a list and shuffle them
+		choicegames = [ownedgame] + othergames
+		shuffle(choicegames)
+		session['correctanswer'] = ownedgame[0]
+		return render_template('play.html',choices=choicegames)
+	else:
+		answer = request.form['answer']
+		if str(answer) == str(session['correctanswer']):
+			resultmsg = "congratulations, gabe is pleased"
+		else:
+			resultmsg = "failure, gabe is displeased"
+		ownedgame = app.ydkbengine.pickOwnedGame(0)
+		othergames = app.ydkbengine.pickOtherGames(2)
+		choicegames = [ownedgame] + othergames
+		shuffle(choicegames)
+		session['correctanswer'] = ownedgame[0]
+		return render_template('play.html',resultmsg=resultmsg,choices=choicegames)
+
+if __name__ == '__main__':
+	app.run(debug=True)
---
+++ app/config.py
@@ -0,0 +1,3 @@
+SECRET_KEY = 'its a secret'
+STEAM_API_KEY = ''
+DEBUG = True
